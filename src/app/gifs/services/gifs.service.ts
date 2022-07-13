82650eefe4fce4726c15c10f4cd218a22342649e
DANIEL AGUDELO MONTOYA <dagudelo483@misena.edu.co>
2022-07-13 16:57:09 -0500

    13 de julio

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,39 @@
+import { query } from '@angular/animations';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey : string = 'Qd3FOYey4MoA3J0bl7qKfRmIRLNFFJ9G';
+  private _historial: string[] = [];
+
+
+  get historial(){
+
+    return [...this._historial];
+  }
+
+
+  constructor (private http: HttpClient){}
+
+
+  buscarGifs( query: string ='') {
+
+    query = query.trim().toLocaleLowerCase();
+
+    if ( !this._historial.includes(query )){
+      this._historial.unshift( query);
+      this._historial = this._historial.splice(0,10);
+    }
+
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=Qd3FOYey4MoA3J0bl7qKfRmIRLNFFJ9G&q=Dragon Ball z&limit=10')
+    .subscribe(resp => {
+            console.log(resp);
+          } )
+
+  }
+
+}
